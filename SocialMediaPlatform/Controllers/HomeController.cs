1f09dac9199c4f2c7669a60d2d87f43e362a16d4
kenshikento <tien-nguyen1@hotmail.co.uk>
2020-02-04 10:12:35 +0000

    Added youtube connection for Api also url helper

--- SocialMediaPlatform/Controllers/HomeController.cs
+++ SocialMediaPlatform/Controllers/HomeController.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SocialMediaPlatform.Support;
+using System.Threading.Tasks;
 
 namespace SocialMediaPlatform.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
+            System.Diagnostics.Debug.WriteLine(123);
+            var instance = new YoutubeService();
+
+            // Dummy playlist 
+            string playListID = "PLH5ogLzrBuBVi4S14Bmwf3VBLc4FfzljG";
+            object hello = await instance.getPlayList(playListID); 
+
+            System.Diagnostics.Debug.WriteLine(instance);
             return View();
 
         }
---
+++ SocialMediaPlatform/Support/YoutubeService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using SocialMediaPlatform.Support.Helpers;
+
+
+namespace SocialMediaPlatform.Support
+{ 
+    public class YoutubeService
+    {
+        public string Creds()
+        {  
+            return "Change it"; // Change this to yours
+        }
+
+        static readonly HttpClient client = new HttpClient();
+
+        static async Task<string> BaseCall(string type, Dictionary<string, string> par)
+        {
+            // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                string baseUrl = "https://www.googleapis.com/youtube/v3/";
+
+                var param = new Helpers.QueryUriBuilder();
+
+                foreach (KeyValuePair<string, string> data in par)
+                {
+                    param.Add(data.Key, data.Value);
+                }
+
+                string Url = baseUrl + type + "?" + param;
+        
+                HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, Url);
+                requestMessage.Headers.Add("Accept", "application / json");
+                HttpResponseMessage response = await client.SendAsync(requestMessage);
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return responseBody;
+            }
+            catch (HttpRequestException e) 
+            {
+                return "Returned Nothing or call terribly wrong" + e;
+            }
+        }
+
+        public async Task<object> getPlayList(string playListID)
+        {
+            string apiKey = Creds();
+            var parm = new Dictionary<string, string>();
+            parm.Add("part","snippet");
+            parm.Add("playlistId", playListID);
+            parm.Add("key", apiKey);
+            string type = "playlistItems";
+
+            object response = await BaseCall(type, parm);
+            // TODO: Need to add Cache into response
+            return response;
+        }
+
+    }
+}
